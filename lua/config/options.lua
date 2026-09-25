03207f1f30458bbbba1d9973d9e7a692a8a78d93
GuillermoX <guillermopinte@gmail.com>
2026-09-25 02:28:24 +0200

    First commit

---
+++ lua/config/options.lua
@@ -0,0 +1,10 @@
+vim.opt.number = true		-- Line numbers
+vim.opt.cursorline = true	-- Cursor line
+vim.opt.relativenumber = true	-- Relative line numbers
+vim.opt.shiftwidth = 4		-- Automatic tab width
+
+
+
+
+
+
---
+++ lua/plugins/oneliners.lua
@@ -0,0 +1,15 @@
+-- Simple pluggins
+return {
+    { -- This helps with ssh tunneling and copying to clipboard
+	'ojroques/vim-oscyank',
+    },
+    { -- Git plugin
+	'tpope/vim-fugitive',
+    },
+    { -- Show CSS colors
+	'brenoprata10/nvim-highlight-colors',
+	config = function()
+	    require('nvim-highlight-colors').setup({})
+	end
+    },
+}
