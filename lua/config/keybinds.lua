41b19d5a5403100043e4d1350f667baa9a22fb61
GuillermoX <guillermopinte@gmail.com>
2026-09-25 02:49:43 +0200

    file explorer

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,2 +1,10 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+-- Neotree
+vim.keymap.set("n", "<leader>e", function()
+  if vim.bo.filetype == "neo-tree" then
+    vim.cmd("wincmd p") -- "wincmd p" salta a la ventana anterior (tu archivo de código)
+  else
+    vim.cmd("Neotree focus") -- Si estás en el código, salta a Neo-tree
+  end
+end, { desc = "Toggle Focus en Neo-tree" })
+vim.keymap.set("n", "<leader>cd", ":Neotree toggle<CR>", { desc = "Toggle File Explorer" }) 	-- Show sidebar
---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,27 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+    config = function()
+	require("neo-tree").setup({
+	    close_if_last_window = true,
+	    window = {
+		position = "left",
+		width = 30, -- Lateral bar width
+	    },
+	    filesystem = {
+		filtered_items = {
+		    visible = true,
+		},
+		follow_current_file = {
+		    enabled = true,
+		},
+	    }
+	})
+    end,
+}
